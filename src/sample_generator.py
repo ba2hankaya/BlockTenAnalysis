3530832ed2896f74c6f75662e9face4931856b4a
ba2hankaya <baa2han.kay@gmail.com>
2026-05-21 23:37:14 +0300

    Added option to save sample into separate files so it can be fed into analyzer without any changes to analyzer code, lazy solution, may streamline later

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -22,20 +22,31 @@ if __name__ == "__main__":
     parser.add_argument('-o','--output-file', type=str, help='Output file name for the generated sample')
     parser.add_argument('-n', '--num-threads', type=int, help='Number of threads to use for generation', default=1)
     parser.add_argument('-c', '--chunk-count', type=int, help='Number of chunks to generate the sample (only a portion of the sample will be kept in memory using number of entries / chunk_count) (default: 1)', default=1)
+    parser.add_argument('-s', '--seperate-files', action='store_true', help='Whether to write each chunk to a seperate file (default: False)')
     args = parser.parse_args()
 
     num_entries = args.num_entries
     output_file = args.output_file
     num_threads = args.num_threads
     chunk_count = args.chunk_count
+    seperate_files = args.seperate_files
+
+    if seperate_files:
+        output_file = output_file[:output_file.rfind('.')] if '.' in output_file else output_file
 
     for i in range(chunk_count):
         base_seed = time.time()
         tasks = [(num_entries // chunk_count // num_threads, base_seed + i) for i in range(num_threads)]
         with multiprocessing.Pool(processes=num_threads) as pool:
             results = pool.starmap(generate_sequences, tasks)
-        with open(output_file, 'a') as f:
-            if i == 0:
-                f.write(f'{num_entries}\n')
-            for result in results:
-                f.writelines(result)
+        if seperate_files:
+            with open(f'{output_file}_chunk_{i}.txt', 'w') as f:
+                f.write(f'{num_entries // chunk_count}\n')
+                for result in results:
+                    f.writelines(result)
+        else:
+            with open(output_file, 'a') as f:
+                if i == 0:
+                    f.write(f'{num_entries}\n')
+                for result in results:
+                    f.writelines(result)
