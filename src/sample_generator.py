9885759e67ecde5b84af523dd4b23db3947b2762
ba2hankaya <baa2han.kay@gmail.com>
2026-05-21 23:24:42 +0300

    Added the option to generate the sample in user inputted chunks so that memory usage is limited

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -21,18 +21,21 @@ if __name__ == "__main__":
     parser.add_argument('num_entries', type=int, help='Number of random card sequences to generate')
     parser.add_argument('-o','--output-file', type=str, help='Output file name for the generated sample')
     parser.add_argument('-n', '--num-threads', type=int, help='Number of threads to use for generation', default=1)
+    parser.add_argument('-c', '--chunk-count', type=int, help='Number of chunks to generate the sample (only a portion of the sample will be kept in memory using number of entries / chunk_count) (default: 1)', default=1)
     args = parser.parse_args()
 
     num_entries = args.num_entries
     output_file = args.output_file
     num_threads = args.num_threads
+    chunk_count = args.chunk_count
 
-    base_seed = time.time()
-    tasks = [(num_entries// num_threads, base_seed + i) for i in range(num_threads)]
-    with multiprocessing.Pool(processes=num_threads) as pool:
-        results = pool.starmap(generate_sequences, tasks)
-
-    with open(output_file, 'w') as f:
-        f.write(f'{num_entries}\n')
-        for result in results:
-            f.writelines(result)
+    for i in range(chunk_count):
+        base_seed = time.time()
+        tasks = [(num_entries // chunk_count // num_threads, base_seed + i) for i in range(num_threads)]
+        with multiprocessing.Pool(processes=num_threads) as pool:
+            results = pool.starmap(generate_sequences, tasks)
+        with open(output_file, 'a') as f:
+            if i == 0:
+                f.write(f'{num_entries}\n')
+            for result in results:
+                f.writelines(result)
